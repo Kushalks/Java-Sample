290eb780c67a3ce5cace2663931d78506e4cd05c
Kushal, K S <kushal_ks@optum.com>
2020-12-31 12:54:14 +0530

    first commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ArrayList<String> al = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            al.add("Kushal");
+        }
+        for(String s:al)
+        {
+            System.out.println(s+" K S");
+        }
+    }
+}
